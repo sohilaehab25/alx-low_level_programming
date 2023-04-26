518790b3139c9281d5c902fd31793035ad7fa0c2
sohilaehab25 <sohailaehab25@gmail.com>
2023-04-26 05:17:57 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-islower.c -o 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
+  *_islower - checks for lowercase character 
+  * @c: the character to be checked
+  * Return: 1 for lowercase character or 0 for anything else
+  */
+int _isLowercase(char c) 
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+    if (c >= 'a' && c <= 'z')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
