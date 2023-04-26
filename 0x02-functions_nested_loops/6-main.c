d4aa54ae27c043ce014c42137d08bfcf1153ab0b
sohilaehab25 <sohailaehab25@gmail.com>
2023-04-26 13:05:00 -0700

    soso

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,21 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code
- * Return: Always 0.
-*/
+ *main-program entry point.
+ *Return:0 no error, non zero if error
+ */
 
 int main(void)
 {
-    int r;
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
+	int i;
 
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
+‏
