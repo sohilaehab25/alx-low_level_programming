3327bb17bc99c966a550d08c574709e8c8afaf4e
sohilaehab25 <sohailaehab25@gmail.com>
2023-04-26 13:49:28 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,22 +3,24 @@
   * print_sign - print the sign of the number
   * @n: the number of the sign to be printed
   * Return: 1 if the number is greater than zero
-  * 0 if number is zero 
-  * -1 if number is less than zero 
+  * 0 if number is zero
+  * -1 if number is less than zero
   */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-	        return (1);
+
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(0);
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
