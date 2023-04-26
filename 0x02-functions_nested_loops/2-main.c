3319886e482732a065039634b830ecfa4549bb56
sohilaehab25 <sohailaehab25@gmail.com>
2023-04-26 06:40:40 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 2-main.c 2-print_alphabet_x10.c -o 2-alphabet_x10

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,13 +1,10 @@
-julien@ubuntu:~/0x02$ cat 2-main.c
 #include "main.h"
-
 /**
  * main - check the code.
- *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet_x10();
-    return (0);
+	print_alphabet_x10();
+	return (0);
 }
